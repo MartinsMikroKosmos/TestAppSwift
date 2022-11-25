ffd3481dd7407f1e78d6270ef2581b971e7c3cd1
Martinus Iudex <martin.richter.1990@gmail.com>
2022-11-25 15:58:41 +0100

    Login

--- TestApp/LoginViewController.swift
+++ TestApp/LoginViewController.swift
@@ -7,22 +7,43 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
     
     @IBOutlet weak var userNameTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     
+    @IBOutlet weak var loginBTN: UIButton!
     
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Tab outside the Keyboard to hide
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    
+        // Keyboard Types and delegate
+        userNameTF.keyboardType = .default
+        userNameTF.delegate = self
+        passwordTF.keyboardType = .default
+        passwordTF.delegate = self
+        
+        // Login Button disable
+        loginBTN.isEnabled = false
+        
     }
+    
+    
+    
 
     @IBAction func LogIn(_ sender: UIButton) {
+        var username = userNameTF.text
+        var password = passwordTF.text
+        
+       
     }
     
 }
---
+++ TestApp/RegisterViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterViewController.swift
+//  TestApp
+//
+//  Created by Martin Richter on 25.11.22.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
